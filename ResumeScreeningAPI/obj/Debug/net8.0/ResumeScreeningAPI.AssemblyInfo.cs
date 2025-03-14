7b055b3f8692d7e40f4265aab1d2f99f57d845e0
19kummitha <113335352+19kummitha@users.noreply.github.com>
2025-03-14 14:51:48 +0530

    changes updated

--- ResumeScreeningAPI/obj/Debug/net8.0/ResumeScreeningAPI.AssemblyInfo.cs
+++ ResumeScreeningAPI/obj/Debug/net8.0/ResumeScreeningAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeScreeningAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e261d2bb8cbd4a3b080e20f6cfcb6148f0a6262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc6ae0d3a3321ef4b16dd5a18c49d2103b242e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeScreeningAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeScreeningAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
